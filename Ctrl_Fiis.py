44879a343e8e27f3d8b9ea4b1387f4c8d32bc161
GaTrani <gabrieltrani02@gmail.com>
2022-12-26 15:35:03 -0300

    Update Ctrl_Fiis.py

--- Ctrl_Fiis.py
+++ Ctrl_Fiis.py
@@ -1,36 +1,13 @@
 from playwright.sync_api import sync_playwright
 
+def run(playwright):
+    #chromium = playwright.chromium() # or "firefox" or "webkit".
+    browser = playwright.chromium.launch(channel="chrome")
+    page = browser.new_page()
+    page.goto("http://google.com")
+    # other actions...
+    #browser.close()
 
-from playwright.sync_api import Page, expect
+with sync_playwright() as playwright:
+    run(playwright)
 
-def teste(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
-
-    # create a locator
-    get_started = page.get_by_role("link", name="Get started")
-
-    # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # Click the get started link.
-    get_started.click()
-
-    # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
-
-
-with sync_playwright() as p:
-    navegador = p.chromium.launch(headless=False) #removendo ou deixando TRUE nao aparecerá o navegador
-    pagina = navegador.new_page()
-
-    ativo = 'URPR11'
-    link = 'https://fiis.com.br/' + ativo + '/'
-    print('Acessando:', link)
-    try:
-        valorAtivo = pagina.locator('xpath=//*[@id="quotations--infos-wrapper"]/div[1]/span[2]')\
-            .text_content(timeout=4000)
-    except:
-        print('erro')
