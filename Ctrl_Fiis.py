2df9f3b4a8d602829b2c2ad3b7c1478a98bca4cc
GaTrani <gabrieltrani02@gmail.com>
2022-12-27 15:04:13 -0300

    Up

--- Ctrl_Fiis.py
+++ Ctrl_Fiis.py
@@ -1,13 +1,40 @@
 from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
+from playwright.sync_api import expect
 
-def run(playwright):
-    #chromium = playwright.chromium() # or "firefox" or "webkit".
-    browser = playwright.chromium.launch(channel="chrome")
-    page = browser.new_page()
-    page.goto("http://google.com")
-    # other actions...
-    #browser.close()
+with sync_playwright() as p:
+    browser = p.chromium.launch(channel="chrome")
+    navegador = p.chromium.launch(headless=False) #removendo ou deixando TRUE nao aparecerá o navegador
+    pagina = navegador.new_page()
+    i = 'URPR11'
+    #link = 'https://fiis.com.br/' + i + '/'
+    link = 'https://statusinvest.com.br/fundos-imobiliarios/afhi11'
+    '''pagina.goto(link)
+    print('Acessando:', link)
+    
+    indicador1 = pagina.locator('//*[@id="main-2"]/div[2]/div[1]').text_content().split()
+    indicador2 = pagina.locator('//*[@id="main-2"]/div[2]/div[5]/div').text_content().split()
+    ultimoRendimento = pagina.locator('//*[@id="dy-info"]').text_content().split()
+    #tabela dividendos
+    dividendos = pagina.locator('//*[@id="earning-section"]/div[7]/div/div[2]/table').text_content()
 
-with sync_playwright() as playwright:
-    run(playwright)
+    cnpj_tipo = pagina.locator('//*[@id="fund-section"]/div/div/div[2]/div').text_content().split()
+    segmento = pagina.locator('//*[@id="fund-section"]/div/div/div[4]/div').text_content().split()'''
+    
+    #evolucao preco
+    link2 = 'https://br.investing.com/equities/af-invest-cri-fii-historical-data'
+    pagina.goto(link2)
+    print('Acessando:', link2)
+    #alterar para mensal
+    pagina.locator('#history-timeframe-selector').click()
+    pagina.keyboard.press('ArrowUp')
+    pagina.keyboard.press('Enter')    
+    historicoPreco = pagina.locator('//*[@id="__next"]/div/div/div/div[2]/main/div/div[8]/div/div/div[3]/div/table/tbody').text_content()
 
+    
+    print('-----', historicoPreco)
+    #print(len(historicoPreco))
+
+    #pagina.close()
+    browser.close()
