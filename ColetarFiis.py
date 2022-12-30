551c2c1e4bd7fd683c446c725532a0e62043e624
GaTrani <gabrieltrani02@gmail.com>
2022-12-30 09:16:33 -0300

    d

--- ColetarFiis.py
+++ ColetarFiis.py
@@ -2,6 +2,7 @@ from playwright.sync_api import sync_playwright
 from playwright.sync_api import expect
 import time
 
+
 with sync_playwright() as p:
     browser = p.chromium.launch(channel="chrome")
     navegador = p.chromium.launch(headless=False) #removendo ou deixando TRUE nao aparecerá o navegador
