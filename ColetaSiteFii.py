4707f7cdd8000d68bb9727a4201e71f528007236
GaTrani <gabrieltrani02@gmail.com>
2023-01-10 12:11:04 -0300

    excecao

--- ColetaSiteFii.py
+++ ColetaSiteFii.py
@@ -34,7 +34,7 @@ with sync_playwright() as p:
     link2 = 'https://fiis.com.br/'
     errosvariados = 0
     erroTimeOut = 0 #len(tickers)
-    for i in range(0, 5):
+    for i in range(0, len(tickers)):
         print('Ativo:', tickers[i])
         link2 = 'https://fiis.com.br/' + tickers[i]
         pagina.goto(link2)
@@ -42,31 +42,23 @@ with sync_playwright() as p:
         linhas = 0
         cont = 0
         try:
-            print('*******************************************')
-            precoAtivo = pagina.locator('//*[@id="carbon_fields_fiis_header-2"]/div/div/div[1]/div[2]/div/div[1]/p[1]/b').inner_text()
-            if(precoAtivo != '0,00'):
-                dados = pagina.locator('//*[@id="carbon_fields_fiis_dividends-2"]/div[2]/div[2]/div/div/div/div/div[2]').inner_text()
-                
-                if(len(dados) > 0):
-                    dados = dados.split()
-                    linhas = int(len(dados) / 7)            
-                    print(tickers[i], dados[0], dados[1], dados[3], dados[4], dados[7])
-                    for l in range(0, linhas):                    
-                        BD.inserirat(tickers[i], dados[cont + 0], dados[cont + 1], dados[cont + 3], dados[cont + 4], dados[cont + 7])
-                        database.commit()
-                        cont += 7
-                print('linha', linhas)            
-            else:
-                print('Ativo com preco 0,00')
-        except TimeoutError:
-            erroTimeOut+=1
-        except:
-            errosvariados+= 1
-            print('-------------------erro try.')
+            print('*******************************************')            
+            dados = pagina.locator('//*[@id="carbon_fields_fiis_dividends-2"]/div[2]/div[2]/div/div/div/div/div[2]').inner_text()
+            if(len(dados) > 0):
+                dados = dados.split()
+                linhas = int(len(dados) / 7)            
+                print(tickers[i], dados[0], dados[1], dados[3], dados[4], dados[7])
+                for l in range(0, linhas):                    
+                    BD.inserirat(tickers[i], dados[cont + 0], dados[cont + 1], dados[cont + 3], dados[cont + 4], dados[cont + 7])
+                    database.commit()
+                    cont += 7
+            print('linha', linhas)
+        except Exception as e:
+                # Handle the exception and continue the loop
+                print(f"Element not found: {e}")
+                continue
         linhas = 0 
-        print('errosVariados', errosvariados)
-        print('errosTimeOut', erroTimeOut)
-
+        
 
     desktop = os.path.abspath("C:\\Users\\gabri\\OneDrive\\Área de Trabalho")
 
